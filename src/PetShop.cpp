70fd7cd1185c2d969f0918edfa5e677a13e89f9b
Ioana Alexandra <148649854+IoanaAlexandraO@users.noreply.github.com>
2025-01-06 20:55:09 +0200

    Update PetShop.cpp

--- src/PetShop.cpp
+++ src/PetShop.cpp
@@ -1,35 +1,54 @@
 #include "PetShop.h"
 #include "CustomException.h"
+#include <algorithm>
 
+PetShop* PetShop::instance = nullptr;
 int PetShop::totalSales = 0;
 
 PetShop& PetShop::getInstance() {
-    static PetShop instance;
-    return instance;
+    if (instance == nullptr) {
+        instance = new PetShop();
+    }
+    return *instance;
 }
 
 void PetShop::addPet(const Pet& pet) {
     pets.push_back(pet);
 }
 
-void PetShop::addCustomer(const Customer& customer) {
+void PetShop::addCustomer(const Customer& customer) { // NOLINT
     customers.push_back(customer);
 }
 
 void PetShop::sellPet(const Pet& pet) {
-    if (pet.getAge() < 0) {
-        throw CustomException();
+    auto it = std::find_if(pets.begin(), pets.end(),
+        [&pet](const Pet& p) { 
+            return p.getName() == pet.getName(); 
+        });
+        
+    if (it != pets.end()) {
+        totalSales += 100;
+        pets.erase(it);
+    } else {
+        throw CustomException("Pet not found!");
     }
-    // Sell the pet
-    totalSales++;
 }
 
-int PetShop::getTotalSales() {
+int PetShop::getTotalSales() { // NOLINT
     return totalSales;
 }
 
-void PetShop::sortPetsByAge() {
-    std::sort(pets.begin(), pets.end(), [](const Pet& a, const Pet& b) {
-        return a.getAge() < b.getAge();
-    });
+void PetShop::sortPetsByAge() { // NOLINT
+    std::sort(pets.begin(), pets.end(),
+        [](const Pet& a, const Pet& b) {
+            return a.getAge() < b.getAge();
+        });
+}
+
+const std::vector<Pet>& PetShop::getPets() const {
+    return pets;
+}
+
+const std::vector<Customer>& PetShop::getCustomers() const {
+    return customers;
 }
