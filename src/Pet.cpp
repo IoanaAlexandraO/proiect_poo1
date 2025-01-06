a0e0b3767f14d79b0a94b8e14bf0a00a158f130d
Ioana Alexandra <148649854+IoanaAlexandraO@users.noreply.github.com>
2025-01-06 20:14:13 +0200

    Update Pet.cpp

--- src/Pet.cpp
+++ src/Pet.cpp
@@ -1,18 +1,21 @@
 #include "Pet.h"
 
-Pet::Pet(const std::string& name, int age) : name(name), age(age) {}
+Pet::Pet(const std::string& name, int age) 
+    : AbstractEntity(), name(name), age(age) {}
 
-Pet::Pet(const Pet& other) : name(other.name), age(other.age) {}
+Pet::Pet(const Pet& other) 
+    : AbstractEntity(other), name(other.name), age(other.age) {}
 
 Pet& Pet::operator=(const Pet& other) {
     if (this != &other) {
+        AbstractEntity::operator=(other);
         name = other.name;
         age = other.age;
     }
     return *this;
 }
 
-Pet::~Pet() {}
+Pet::~Pet() override {}
 
 void Pet::speak() const {
     std::cout << "Pet " << name << " says: Meow!" << std::endl;
