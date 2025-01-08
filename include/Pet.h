f555328d9ceacdf3930a7c3263cf7b7869bf7782
Ioana Alexandra <148649854+IoanaAlexandraO@users.noreply.github.com>
2025-01-08 12:34:29 +0200

    Update Pet.h

--- include/Pet.h
+++ include/Pet.h
@@ -4,19 +4,53 @@
 #include <string>
 #include <iostream>
 
+template<typename T>
 class Pet : public AbstractEntity {
 private:
     std::string name;
-    int age;
+    T age;
 public:
-    Pet(const std::string& name, int age);
+    Pet(const std::string& name, T age);
     Pet(const Pet& other);
     Pet& operator=(const Pet& other);
     ~Pet() override;
-    // cppcheck-suppress unusedFunction
     void speak() const override;
-    int getAge() const;
-     const std::string& getName() const { return name; }
-
+    T getAge() const;
+    std::string getName() const; // Metodă nouă pentru a obține numele
 };
+
+// Implementarea metodelor
+template<typename T>
+Pet<T>::Pet(const std::string& name, T age) : name(name), age(age) {}
+
+template<typename T>
+Pet<T>::Pet(const Pet& other) : name(other.name), age(other.age) {}
+
+template<typename T>
+Pet<T>& Pet<T>::operator=(const Pet& other) {
+    if (this != &other) {
+        name = other.name;
+        age = other.age;
+    }
+    return *this;
+}
+
+template<typename T>
+Pet<T>::~Pet() {}
+
+template<typename T>
+void Pet<T>::speak() const {
+    std::cout << "Pet " << name << " says: Meow!" << std::endl;
+}
+
+template<typename T>
+T Pet<T>::getAge() const {
+    return age;
+}
+
+template<typename T>
+std::string Pet<T>::getName() const {
+    return name;
+}
+
 #endif
