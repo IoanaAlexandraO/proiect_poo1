72f17c7c98c4b4aebe6325599e00d38da906a7af
Ioana Alexandra <148649854+IoanaAlexandraO@users.noreply.github.com>
2025-01-08 12:45:30 +0200

    Update Customer.h

--- include/Customer.h
+++ include/Customer.h
@@ -5,19 +5,25 @@
 #include <string>
 #include <iostream>
 
+template<typename T>
 class Customer : public AbstractEntity {
 private:
     std::string name;
-    int age;
+    T age;
 public:
-    Customer(const std::string& name, int age);
-    Customer(const Customer& other);
-    Customer& operator=(const Customer& other);
-    ~Customer() override;
-    void speak() const override;
-    friend std::ostream& operator<<(std::ostream& os, const Customer& customer);
-    friend std::istream& operator>>(std::istream& is, Customer& customer);
-    bool operator==(const Customer& other) const;
+    Customer(const std::string& name, T age) : name(name), age(age) {}
+    
+    void speak() const override {
+        std::cout << "Customer " << name << " says: Hello!" << std::endl;
+    }
+    
+    T getAge() const {
+        return age;
+    }
+    
+    std::string getName() const {
+        return name;
+    }
 };
 
 #endif
